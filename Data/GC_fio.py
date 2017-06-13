Fixed Figures and GC_output tropchem scaling

Data/GC_fio.py
@@ -8,7 +8,7 @@ Run from main project directory or else imports will not work
 ## Modules
 
 # module for hdf eos 5
-#import h5py 
+#import h5py
 import netCDF4 as nc
 import numpy as np
 from datetime import datetime, timedelta
@@ -18,52 +18,57 @@ from glob import glob # for file pattern reading
 #####GLOBALS######
 ##################
 run_number={"tropchem":0,"UCX":1}
+#folder_location="/home/574/jwg574/OMI_regridding/"
+# relative paths (for use on non NCI computers)
+folder_location="Data/GC_Output/"
 runs=["geos5_2x25_tropchem","UCX_geos5_2x25"]
-paths=["/home/574/jwg574/OMI_regridding/Data/GC_Output/%s/trac_avg"%rstr for rstr in runs]
+#paths=["/home/574/jwg574/OMI_regridding/Data/GC_Output/%s/trac_avg"%rstr for rstr in runs]
+paths=["%s%s/trac_avg"%(folder_location,rstr) for rstr in runs]
 hemcodiag_path="/home/574/jwg574/OMI_regridding/Data/GC_Output/%s/hemco_diags"%runs[run_number['tropchem']]
 
 tropchem_dims=['Tau', 'Pressure', 'latitude', 'longitude']
-tropchem_keys=tropchem_dims+['ANTHSRCENO', 'ANTHSRCECO', 'ANTHSRCEALK4', 
+tropchem_keys=tropchem_dims+['ANTHSRCENO', 'ANTHSRCECO', 'ANTHSRCEALK4',
     'ANTHSRCEACET', 'ANTHSRCEMEK', 'ANTHSRCEALD2', 'ANTHSRCEPRPE', 'ANTHSRCEC3H8',
     'ANTHSRCECH2O', 'ANTHSRCEC2H6', 'BIOBSRCENO', 'BIOBSRCECO', 'BIOBSRCEALK4',
     'BIOBSRCEACET', 'BIOBSRCEMEK', 'BIOBSRCEALD2', 'BIOBSRCEPRPE', 'BIOBSRCEC3H8',
     'BIOBSRCECH2O', 'BIOBSRCEC2H6', 'BIOBSRCESO2', 'BIOBSRCENH3', 'BIOBSRCEBC',
-    'BIOBSRCEOC', 'BIOFSRCENO', 'BIOFSRCECO', 'BIOFSRCEALK4', 'BIOFSRCEACET', 
-    'BIOFSRCEMEK', 'BIOFSRCEALD2', 'BIOFSRCEPRPE', 'BIOFSRCEC3H8', 'BIOFSRCECH2O', 
-    'BIOFSRCEC2H6', 'BIOFSRCESO2', 'BIOFSRCENH3', 'BIOGSRCEISOP', 'BIOGSRCEACET', 
-    'BIOGSRCEPRPE', 'BIOGSRCEMONX', 'BIOGSRCEMBOX', 'BIOGSRCEAPIN', 'BIOGSRCEBPIN', 
-    'BIOGSRCELIMO', 'BIOGSRCESABI', 'BIOGSRCEMYRC', 'BIOGSRCECARE', 'BIOGSRCEOCIM', 
-    'BIOGSRCEHCOOH', 'BIOGSRCEACTA', 'BIOGSRCEALD2', 'BIOGSRCEOMON', 'BIOGSRCEMOHX', 
-    'BIOGSRCEETOH', 'BIOGSRCEFARN', 'BIOGSRCEBCAR', 'BIOGSRCEOSQT', 'BIOGSRCECHBr3', 
-    'BIOGSRCECH2Br2', 'BIOGSRCESSBr2', 'BXHGHT-$BXHEIGHT', 'BXHGHT-$AD', 
-    'BXHGHT-$AVGW', 'BXHGHT-$N(AIR)', 'CH4-EMISCH4-TOT', 'CH4-EMISCH4-GAO', 
-    'CH4-EMISCH4-COL', 'CH4-EMISCH4-LIV', 'CH4-EMISCH4-WST', 'CH4-EMISCH4-BFL', 
+    'BIOBSRCEOC', 'BIOFSRCENO', 'BIOFSRCECO', 'BIOFSRCEALK4', 'BIOFSRCEACET',
+    'BIOFSRCEMEK', 'BIOFSRCEALD2', 'BIOFSRCEPRPE', 'BIOFSRCEC3H8', 'BIOFSRCECH2O',
+    'BIOFSRCEC2H6', 'BIOFSRCESO2', 'BIOFSRCENH3', 'BIOGSRCEISOP', 'BIOGSRCEACET',
+    'BIOGSRCEPRPE', 'BIOGSRCEMONX', 'BIOGSRCEMBOX', 'BIOGSRCEAPIN', 'BIOGSRCEBPIN',
+    'BIOGSRCELIMO', 'BIOGSRCESABI', 'BIOGSRCEMYRC', 'BIOGSRCECARE', 'BIOGSRCEOCIM',
+    'BIOGSRCEHCOOH', 'BIOGSRCEACTA', 'BIOGSRCEALD2', 'BIOGSRCEOMON', 'BIOGSRCEMOHX',
+    'BIOGSRCEETOH', 'BIOGSRCEFARN', 'BIOGSRCEBCAR', 'BIOGSRCEOSQT', 'BIOGSRCECHBr3',
+    'BIOGSRCECH2Br2', 'BIOGSRCESSBr2', 'BXHGHT-$BXHEIGHT', 'BXHGHT-$AD',
+    'BXHGHT-$AVGW', 'BXHGHT-$N(AIR)', 'CH4-EMISCH4-TOT', 'CH4-EMISCH4-GAO',
+    'CH4-EMISCH4-COL', 'CH4-EMISCH4-LIV', 'CH4-EMISCH4-WST', 'CH4-EMISCH4-BFL',
     'CH4-EMISCH4-RIC', 'CH4-EMISCH4-OTA', 'CH4-EMISCH4-BBN', 'CH4-EMISCH4-WTL',
-    'CH4-EMISCH4-SAB', 'CH4-EMISCH4-OTN', 'DRYD-FLXO3df', 'DRYD-FLXCH2Odf', 
-    'DRYD-VELO3dv', 'DRYD-VELCH2Odv', 'DXYPDXYP', 'IJ-AVG-$NO', 'IJ-AVG-$O3', 
-    'IJ-AVG-$ISOP', 'IJ-AVG-$MVK', 'IJ-AVG-$MACR', 'IJ-AVG-$CH2O', 'IJ-AVG-$ISOPN', 
-    'IJ-AVG-$RIP', 'IJ-AVG-$IEPOX', 'IJ-AVG-$NO2', 'IJ-AVG-$NO3', 'JV-MAP-$JHNO3', 
-    'JV-MAP-$', 'PBLDEPTHPBL-M', 'PBLDEPTHPBL-L', 'PEDGE-$PSURF', 'PORL-L=$POX', 
+    'CH4-EMISCH4-SAB', 'CH4-EMISCH4-OTN', 'DRYD-FLXO3df', 'DRYD-FLXCH2Odf',
+    'DRYD-VELO3dv', 'DRYD-VELCH2Odv', 'DXYPDXYP', 'IJ-AVG-$NO', 'IJ-AVG-$O3',
+    'IJ-AVG-$ISOP', 'IJ-AVG-$MVK', 'IJ-AVG-$MACR', 'IJ-AVG-$CH2O', 'IJ-AVG-$ISOPN',
+    'IJ-AVG-$RIP', 'IJ-AVG-$IEPOX', 'IJ-AVG-$NO2', 'IJ-AVG-$NO3', 'JV-MAP-$JHNO3',
+    'JV-MAP-$', 'PBLDEPTHPBL-M', 'PBLDEPTHPBL-L', 'PEDGE-$PSURF', 'PORL-L=$POX',
     'PORL-L=$LOX', 'TR-PAUSETP-LEVEL', 'TR-PAUSETP-HGHT', 'TR-PAUSETP-PRESS' ]
-tropchem_HCHO_keys=tropchem_dims+['BXHGHT-$BXHEIGHT', 'BXHGHT-$AD', 'BXHGHT-$AVGW', 
-    'BXHGHT-$N(AIR)', 'DXYPDXYP', 'IJ-AVG-$ISOP', 'IJ-AVG-$CH2O', 'PEDGE-$PSURF', 
+tropchem_HCHO_keys=tropchem_dims+['BXHGHT-$BXHEIGHT', 'BXHGHT-$AD', 'BXHGHT-$AVGW',
+    'BXHGHT-$N(AIR)', 'DXYPDXYP', 'IJ-AVG-$ISOP', 'IJ-AVG-$CH2O', 'PEDGE-$PSURF',
     'TR-PAUSETP-LEVEL']
 tropchem_Isop_keys=tropchem_HCHO_keys+['IJ-AVG-$ISOP', 'IJ-AVG-$MVK', 'IJ-AVG-$MACR',
     'IJ-AVG-$NO2']
 # SOME KEYS DIDN'T GET THE 1E9 SCALING !?
 tropchem_scaled_keys=['IJ-AVG-$ISOP','IJ-AVG-$CH2O']
+tropchem_scale=1e9
 
 UCX_dims=['time','lev','lat','lon']
 UCX_keys=UCX_dims+['IJ_AVG_S__O3','IJ_AVG_S__ISOP','IJ_AVG_S__MVK','IJ_AVG_S__MACR',
     'IJ_AVG_S__CH2O','PEDGE_S__PSURF','CHEM_L_S__LBRO2H','PORL_L_S__POX',
-    'BXHGHT_S__BXHEIGHT','BXHGHT_S__AD','BXHGHT_S__AVGW','BXHGHT_S__N_AIR_', 
+    'BXHGHT_S__BXHEIGHT','BXHGHT_S__AD','BXHGHT_S__AVGW','BXHGHT_S__N_AIR_',
     'DXYP__','TR_PAUSE__TP_LEVEL','TR_PAUSE__TP_HGHT',
     'TR_PAUSE__TP_PRESS','CH4_LOSS__']
 UCX_HCHO_keys=UCX_dims+['IJ_AVG_S__ISOP','IJ_AVG_S__CH2O','PEDGE_S__PSURF',
     'BXHGHT_S__BXHEIGHT','BXHGHT_S__AD','BXHGHT_S__AVGW','BXHGHT_S__N_AIR_',
     'DXYP__','TR_PAUSE__TP_LEVEL']
 UCX_Isop_keys=UCX_HCHO_keys+['IJ_AVG_S__ISOP','IJ_AVG_S__MVK','IJ_AVG_S__MACR']
-    
+
 UCX_scaled_keys=[]
 __VERBOSE__=False
 
@@ -72,7 +77,7 @@ __VERBOSE__=False
 ################
 
 def date_from_gregorian(greg):
-    ''' 
+    '''
         gregorian = "hours since 1985-1-1 00:00:0.0"
         Returns list of datetimes
     '''
@@ -128,7 +133,7 @@ def read_UCX():
     print("Reading %s"%fullpath)
     ucxfile=nc.Dataset(fullpath,'r')
     return(ucxfile)
- 
+
 def read_tropchem(date=datetime(2005,1,1)):
     ''' read output file'''
     dstr=date.strftime("%Y%m%d")
@@ -139,7 +144,7 @@ def read_tropchem(date=datetime(2005,1,1)):
     tropfile=nc.Dataset(fullpath,'r')
     return(tropfile)
 
-def get_tropchem_data(date=datetime(2005,1,1), keys=tropchem_HCHO_keys, 
+def get_tropchem_data(date=datetime(2005,1,1), keys=tropchem_HCHO_keys,
     monthavg=True, surface=False):
     ''' return a subset of the tropchem data '''
     tf=read_tropchem(date)
@@ -151,12 +156,12 @@ def get_tropchem_data(date=datetime(2005,1,1), keys=tropchem_HCHO_keys,
     lon     = tf.variables['longitude'][:]
     Tau     = tf.variables['Tau'][:] # Tau(time): hours since 19850101:0000
     Press   = tf.variables['Pressure'][:] # Pressure(altitude): hPa, midpoints
-    
+
     # get the subset of data: (most are [[lev],lat,lon,time])
     data={}
-    
+
     for key in keys:
-        scale=[1.0, 1e9][key in tropchem_scaled_keys] # scale if needed
+        scale=[1.0, tropchem_scale][key in tropchem_scaled_keys] # scale if needed
         tmp=tf.variables[key][:]*scale
         if __VERBOSE__:
             print("%s originally [%s], with min, mean, max = (%.2e, %.2e %.2e)"%
@@ -190,9 +195,9 @@ def get_tropchem_data(date=datetime(2005,1,1), keys=tropchem_HCHO_keys,
     return(data)
 
 def read_HEMCO_diag(date=datetime(2005,1,1)):
-    ''' 
-        Read the HEMCO_diagnostics BIOGENIC ISOP EMISSIONS data 
-        This is for tropchem run only, with output in units 
+    '''
+        Read the HEMCO_diagnostics BIOGENIC ISOP EMISSIONS data
+        This is for tropchem run only, with output in units
         float ISOP_BIOG(time, lat, lon) ;
             ISOP_BIOG:long_name = "ISOP_BIOG" ;
             ISOP_BIOG:units = "kg/m2/s" ;
@@ -213,10 +218,10 @@ def read_HEMCO_diag(date=datetime(2005,1,1)):
         	float time(time) ;
         		time:long_name = "Time" ;
         		time:units = "hours since 1985-01-01 00:00:00" ;
-                
-            
+
+
     '''
-    # looks like hemco_diags/hemco_diags.200502.nc 
+    # looks like hemco_diags/hemco_diags.200502.nc
     dstr=date.strftime("%Y%m")
     fname="%s/hemco_diags.%s.nc"%(hemcodiag_path,dstr)
     print("Reading %s"%fname)
@@ -233,10 +238,10 @@ def get_UCX_data(date=datetime(2005,1,1), keys=UCX_HCHO_keys, surface=False):
     lon     = uf.variables['lon'][:]
     Tau     = uf.variables['time'][:] # Tau(time): hours since 19850101:0000
     Press   = uf.variables['lev'][:] # Pressure(altitude): hPa, midpoints
-    
+
     # get the subset of data: (most are [time,[lev],lat,lon])
     data={}
-    
+
     # find the month index:
     di=index_from_gregorian(Tau,date)
     if __VERBOSE__:
@@ -250,7 +255,7 @@ def get_UCX_data(date=datetime(2005,1,1), keys=UCX_HCHO_keys, surface=False):
                 (key, str(tmp.shape), np.nanmin(tmp), np.nanmean(tmp), np.nanmax(tmp)))
         dims=np.shape(tmp)
         Tdim=0 # time is first dimension
-        
+
         if dims[Tdim] == len(Tau): # check we have the time dimension
             tmp=np.squeeze(tmp[di])
             if __VERBOSE__:
@@ -271,7 +276,7 @@ def get_UCX_data(date=datetime(2005,1,1), keys=UCX_HCHO_keys, surface=False):
         data[key]=tmp
         if __VERBOSE__:
             print("%s now has shape: %s"%(key,str(np.shape(data[key]))))
-        
+
     # return the data we want
     uf.close()
     return(data)
@@ -287,10 +292,10 @@ def determine_trop_column(ppbv, N_air, boxH, tplev):
             tropcol[lat,lon]: tropospheric column (molec/cm2)
     '''
     dims=np.shape(ppbv)
-    
+
     # (molec_x/1e9 molec_air) * 1e9 * molec_air/m3 * m * m2/cm2
     X = ppbv * 1e-9 * N_air * boxH * 1e-4 # molec/cm2
-        
+
     out=np.zeros([dims[1],dims[2]])
     for lat in range(dims[1]):
         for lon in range(dims[2]):
@@ -299,7 +304,7 @@ def determine_trop_column(ppbv, N_air, boxH, tplev):
             out[lat,lon]= np.sum(X[0:trop,lat,lon]) + extra*X[trop,lat,lon]
     return out
 
-    
+
 def _test_trop_column_calc():
     '''
     This tests the trop column calculation with a trivial case

GC_class.py
@@ -23,7 +23,7 @@ from JesseRegression import RMA
 #####GLOBALS######
 ##################
 # map for gc output names to simple names, and then reversed
-Simplenames={'Tau':'taus','Pressure':'press','latitude':'lats','longitude':'lons', 
+Simplenames={'Tau':'taus','Pressure':'press','latitude':'lats','longitude':'lons',
     'BXHGHT-$BXHEIGHT':'boxH','BXHGHT-$N(AIR)':'N_air','DXYPDXYP':'area',
     'IJ-AVG-$ISOP':'isop', 'IJ-AVG-$CH2O':'hcho', 'PEDGE-$PSURF':'psurf',
     'TR-PAUSETP-LEVEL':'tplev', 'IJ-AVG-$MVK':'mvk', 'IJ-AVG-$MACR':'macr',
@@ -48,7 +48,7 @@ class GC_output:
         # tropchem dims [[lev,] lat, lon[, time]]
         # UCX dims: lev = 72; alt059 = 59; lat = 91; lon = 144;
         self.hcho  = PPBV
-        self.isop  = PPBV or PPBC ???
+        self.isop  = PPBC (=PPBV*5)
         self.boxH  = box heights (m)
         self.psurf = pressure surfaces (hPa)
         self.area  = XY grid area (m2)
@@ -59,7 +59,7 @@ class GC_output:
     def __init__(self, date, UCX=False):
         ''' Read data for date into self '''
         self.dstr=date.strftime("%Y%m")
-        
+
         # READ DATA, Tropchem or UCX file
         self.UCX=False
         if UCX:
@@ -68,44 +68,52 @@ class GC_output:
                                          surface=False)
             for key,val in SimpleUCXnames.items():
                 setattr(self, val, tavg_data[key])
+
             self.taus=gcfio.gregorian_from_dates([date])
-        
+
         else: # READ TROPCHEM DATA:
             tavg_file=gcfio.read_tropchem(date)
-            # Save all keys in Simplenames dict to class using associated simple names:
+            # Save data using names mapped from the Simplenames dict:
             for key,val in Simplenames.items():
-                setattr(self, val, tavg_file.variables[key][:])
+                # Some tropchem attributes lost the 1e9 scaling
+                # during the bpch2coards process
+                if key in gcfio.tropchem_scaled_keys:
+                    setattr(self, val, tavg_file.variables[key][:]*gcfio.tropchem_scale)
+                else:
+                    setattr(self, val, tavg_file.variables[key][:])
+
+            #Close the file
             tavg_file.close()
-        
+
         # add some peripheral stuff
         self.n_lats=len(self.lats)
         self.n_lons=len(self.lons)
-        
+
         # set dates and E_dates:
         self.dates=gcfio.date_from_gregorian(self.taus)
-        
-    
+
+
     def get_trop_columns(self, keys=['hcho'], metres=False):
         ''' Return tropospheric column amounts in molec/cm2 [or molec/m2] '''
         data={}
-        
+
         # where is tropopause and how much of next box we want
         trop=np.floor(self.tplev).astype(int)
         extra=self.tplev - trop
-        
+
         # for each key, work out trop columns
         for key in keys:
             ppbv=getattr(self,key)
             # if key is Isoprene: we have PPBC instead of PPBV
             if key=='isop':
                 ppbv=ppbv/5.0 # convert PPBcarbon to PPBisoprene
-            
+
             dims=np.shape(ppbv)
-            
+
             # ppbv * 1e-9 * molec_air/m3 * m * [m2/cm2]
-            scale=[1e-4,1.0][metres]
+            scale=[1e-4, 1.0][metres]
             X = ppbv * 1e-9 * self.N_air * self.boxH * scale # molec/area
-            
+
             out=np.zeros(dims[1:])
             for lat in range(dims[1]):
                 for lon in range(dims[2]):
@@ -116,10 +124,10 @@ class GC_output:
                         for t in range(dims[3]):
                             tropi=trop[lat,lon,t]
                             out[lat,lon,t]= np.sum(X[0:tropi,lat,lon,t])+extra[lat,lon,t]*X[tropi,lat,lon,t]
-                        
+
             data[key]=out
         return data
-    
+
     def month_average(self, keys=['hcho','isop']):
         ''' Average the time dimension '''
         out={}
@@ -134,7 +142,7 @@ class GC_output:
             dims=np.shape(attr)
             if (dims[-1]==n_t) or (dims[-1]==len(self.E_taus)):
                 out[v]=np.nanmean(attr, axis=len(dims)-1) # average the last dimension
-        
+
         return out
     def get_surface(self, keys=['hcho']):
         ''' Get the surface layer'''
@@ -142,7 +150,7 @@ class GC_output:
         for v in keys:
             out[v]=(getattr(self,v))[0]
         return out
-    
+
     def _get_region(self,aus=False, region=None):
         ''' region for plotting '''
         if region is None:
@@ -150,8 +158,8 @@ class GC_output:
             if aus:
                 region=__AUSREGION__
         return region
- 
-    
+
+
 ################
 ###FUNCTIONS####
 ################
@@ -162,31 +170,31 @@ def check_units():
         boxH (m)
         trop_cols (molecs/cm2)
         E_isop (TODO)
-        
+
     '''
     N_ave=6.02214086*1e23 # molecs/mol
     airkg= 28.97*1e-3 # ~ kg/mol of dry air
-    gc=GC_tropchem(datetime(2005,1,1))
+    gc=GC_output(datetime(2005,1,1))
     # N_air is molec/m3 in User manual, and ncfile: check it's sensible:
     nair=np.mean(gc.N_air[0]) # surface only
     airmass=nair/N_ave * airkg  # kg/m3 at surface
     print("Mean surface N_air=%e molec/m3"%nair)
     print(" = %.3e mole/m3, = %4.2f kg/m3"%(nair/N_ave, airmass ))
     assert (airmass > 0.9) and (airmass < 1.5), "surface airmass should be around 1.25kg/m3"
-     
+
     # Boxheight is in metres in User manual and ncfile: check surface height
-    print("Mean surface boxH=%.4f"%np.mean(gc.boxH[0]))
+    print("Mean surface boxH=%.2fm"%np.mean(gc.boxH[0]))
     assert (np.mean(gc.boxH[0]) > 10) and (np.mean(gc.boxH[0]) < 500), "surface level should be around 100m"
-    
+
     # Isop is ppbC in manual , with 5 mole C / mole tracer (?), and 12 g/mole
     trop_cols=gc.get_trop_columns(keys=['hcho','isop'])
     trop_isop=trop_cols['isop']
-    print("Tropospheric isoprene %s mean = %e molec/cm2"%(str(trop_isop.shape),np.nanmean(trop_isop)))
-    print("What's expected for this?")
+    print("Tropospheric isoprene %s mean = %.2e molec/cm2"%(str(trop_isop.shape),np.nanmean(trop_isop)))
+    print("What's expected for this ~1e12?")
     trop_hcho=trop_cols['hcho']
-    print("Tropospheric HCHO %s mean = %e molec/cm2"%(str(trop_hcho.shape),np.nanmean(trop_hcho)))
-    print("What's expected for this?")
-    
+    print("Tropospheric HCHO %s mean = %.2e molec/cm2"%(str(trop_hcho.shape),np.nanmean(trop_hcho)))
+    print("What's expected for this ~1e15?")
+
     # E_isop is atom_C/cm2/s, around 5e12?
 
 def check_diag():
@@ -202,4 +210,4 @@ def check_diag():
 if __name__=='__main__':
     #check_diag()
     check_units()
-    
+

fio.py
@@ -12,7 +12,7 @@ Writes HDF5 files (used to create regridded and reprocessed datasets)
 # plotting module, and something to prevent using displays(can save output but not display it)
 
 # module for hdf eos 5
-import h5py 
+import h5py
 import numpy as np
 from datetime import datetime, timedelta
 from glob import glob
@@ -38,7 +38,7 @@ _omhchorp_keylist=['AMF_GC', 'AMF_GCz', 'AMF_OMI','AMF_PP', 'SC', 'VC_GC', 'VC_O
 
 def save_to_hdf5(outfilename, arraydict, fillvalue=np.NaN, verbose=False):
     '''
-    Takes a bunch of arrays, named in the arraydict parameter, and saves 
+    Takes a bunch of arrays, named in the arraydict parameter, and saves
     to outfilename as hdf5 using h5py with fillvalue=0, gzip compression
     '''
     if verbose:
@@ -51,17 +51,17 @@ def save_to_hdf5(outfilename, arraydict, fillvalue=np.NaN, verbose=False):
         f.attrs['HDF5_Version']     = h5py.version.hdf5_version
         f.attrs['h5py_version']     = h5py.version.version
         f.attrs['Fill Value']       = fillvalue
-        
+
         if verbose:
             print("Inside fio.save_to_hdf5()")
             print(arraydict.keys())
-        
+
         for name in arraydict.keys():
             # create dataset, using arraydict values
             darr=arraydict[name]
             if verbose:
                 print((name, darr.shape, darr.dtype))
-            
+
             # Fill array using darr,
             # this way takes minutes to save, using ~ 500 MB space / avg
             dset=f.create_dataset(name,fillvalue=fillvalue,
@@ -88,7 +88,7 @@ def determine_filepath(date, latres=0.25,lonres=0.3125, gridded=False, regridded
     '''
     Make filename based on date, resolution, and type of file.
     '''
-    
+
     # if not created by me just return the filepath(s) using date variable and glob
     if gridded:
         return glob('Data/omhchog/OMI-Aura*%4dm%02d%02d*.he5'%(date.year, date.month, date.day))[0]
@@ -96,11 +96,11 @@ def determine_filepath(date, latres=0.25,lonres=0.3125, gridded=False, regridded
         return ('Data/omhchorp/metadata/metadata_%s.he5'%(date.strftime('%Y%m%d')))
     if not (regridded or reprocessed):
         return glob('Data/omhcho/OMI-Aura*%4dm%02d%02d*'%(date.year, date.month, date.day))
-    
+
     # geos chem output created via idl scripts match the following
     if geoschem:
         return ('Data/gchcho/hcho_%4d%2d.he5'%(date.year,date.month))
-    
+
     # reprocessed and regridded match the following:
     avg=['8','1'][oneday] # one or 8 day average when applicable
     typ=['p','g'][regridded] # reprocessed or regridded
@@ -116,36 +116,36 @@ def read_8dayfire(date=datetime(2005,1,1,0)):
     '''
     # filenames are all like *yyyyddd.h5, where ddd is day of the year(DOY), one for every 8 days
     tt = date.timetuple()
-    
+
     # only every 8 days matches a file
     # this will give us a multiple of 8 which matches our DOY
-    daymatch= int(np.floor(tt.tm_yday/8)*8) +1 
+    daymatch= int(np.floor(tt.tm_yday/8)*8) +1
     filepath='Data/MYD14C8H/MYD14C8H.%4d%03d.h5' % (tt.tm_year, daymatch)
     return read_8dayfire_path(filepath)
 
 def read_8dayfire_path(path):
     '''
     Read fires file using given path
-    '''    
+    '''
     ## Fields to be read:
     # Count of fires in each grid box over 8 days
     corrFirePix = 'CorrFirePix'
     cloudCorrFirePix = 'CloudCorrFirePix'
-    
+
     ## read in file:
     with h5py.File(path,'r') as in_f:
         ## get data arrays
         cfp     = in_f[corrFirePix].value
         ccfp    = in_f[cloudCorrFirePix].value
-    
+
     # from document at
     # http://www.fao.org/fileadmin/templates/gfims/docs/MODIS_Fire_Users_Guide_2.4.pdf
     # latitude = 89.75 - 0.5 * y
-    # longitude = -179.75 + 0.5 * x 
+    # longitude = -179.75 + 0.5 * x
     lats    = np.arange(90,-90,-0.5) - 0.25
     lons    = np.arange(-180,180, 0.5) + 0.25
     return (cfp, lats, lons)
-    
+
 def read_8dayfire_interpolated(date,latres,lonres):
     '''
     Read the date, interpolate data to match lat/lon resolution, return data
@@ -154,13 +154,13 @@ def read_8dayfire_interpolated(date,latres,lonres):
     fires, lats, lons = read_8dayfire(date)
     #lats = np.arange(90,-90,-0.5) - 0.25
     #lons = np.arange(-180,180, 0.5) + 0.25
-    
+
     newlats= np.arange(-90,90, latres) + latres/2.0
     newlons= np.arange(-180,180, lonres) + lonres/2.0
 
     mlons,mlats = np.meshgrid(lons,lats)
-    mnewlons,mnewlats = np.meshgrid(newlons,newlats)    
-    
+    mnewlons,mnewlats = np.meshgrid(newlons,newlats)
+
     interp = griddata( (mlats.ravel(), mlons.ravel()), fires.ravel(), (mnewlats, mnewlons), method='nearest')
     return interp, newlats, newlons
 
@@ -175,9 +175,9 @@ def read_omhcho(path, szamax=60, screen=[-5e15, 1e17], maxlat=None, verbose=Fals
             'qualityflag':qf, 'xtrackflag':xqf,
             'coluncertainty':cunc, 'convergenceflag':fcf, 'fittingRMS':frms}
     '''
-    
+
     # Total column amounts are in molecules/cm2
-    field_hcho  = datafields+'ColumnAmount' 
+    field_hcho  = datafields+'ColumnAmount'
     field_ref_c = datafields+'RadianceReferenceColumnAmount'
     # other useful fields
     field_amf   = datafields+'AirMassFactor'
@@ -198,8 +198,8 @@ def read_omhcho(path, szamax=60, screen=[-5e15, 1e17], maxlat=None, verbose=Fals
     field_colUnc    = datafields+'ColumnUncertainty' # also molec/cm2
     field_fitflag   = datafields+'FitConvergenceFlag'
     field_fitRMS    = datafields+'FittingRMS'
-    
-    
+
+
     ## read in file:
     with h5py.File(path,'r') as in_f:
         ## get data arrays
@@ -207,7 +207,7 @@ def read_omhcho(path, szamax=60, screen=[-5e15, 1e17], maxlat=None, verbose=Fals
         lons    = in_f[field_lon].value     #
         hcho    = in_f[field_hcho].value    #
         rsc_omi = in_f[field_rsc].value     # ref sector corrected vc
-        amf     = in_f[field_amf].value     # 
+        amf     = in_f[field_amf].value     #
         amfg    = in_f[field_amfg].value    # geometric amf
         clouds  = in_f[field_clouds].value  # cloud fraction
         ctp     = in_f[field_ctp].value     # cloud top pressure
@@ -215,7 +215,7 @@ def read_omhcho(path, szamax=60, screen=[-5e15, 1e17], maxlat=None, verbose=Fals
         xqf     = in_f[field_xqf].value     # cross track flag
         sza     = in_f[field_sza].value     # solar zenith angle
         vza     = in_f[field_vza].value     # viewing zenith angle
-        
+
         # uncertainty arrays                #
         cunc    = in_f[field_colUnc].value  # uncertainty
         fcf     = in_f[field_fitflag].value # convergence flag
@@ -226,9 +226,9 @@ def read_omhcho(path, szamax=60, screen=[-5e15, 1e17], maxlat=None, verbose=Fals
         plevs   = in_f[field_plevs].value   # pressure dim
         #                                   # [ 60 ]
         ref_c   = in_f[field_ref_c].value   # reference radiance col for swath tracks
-        
+
         #
-        ## remove missing values and bad flags: 
+        ## remove missing values and bad flags:
         # QF: missing<0, suss=1, bad=2
         if verbose:
             print("%d pixels in %s prior to filtering"%(np.sum(~np.isnan(hcho)),path))
@@ -239,7 +239,7 @@ def read_omhcho(path, szamax=60, screen=[-5e15, 1e17], maxlat=None, verbose=Fals
         lats[suss] = np.NaN
         lons[suss] = np.NaN
         amf[suss]  = np.NaN
-        
+
         # remove xtrack flagged data
         xsuss       = xqf != 0
         if verbose:
@@ -249,7 +249,7 @@ def read_omhcho(path, szamax=60, screen=[-5e15, 1e17], maxlat=None, verbose=Fals
         lats[xsuss] = np.NaN
         lons[xsuss] = np.NaN
         amf[xsuss]  = np.NaN
-        
+
         # remove pixels polewards of maxlat
         if maxlat is not None:
             with np.errstate(invalid='ignore'):
@@ -261,7 +261,7 @@ def read_omhcho(path, szamax=60, screen=[-5e15, 1e17], maxlat=None, verbose=Fals
             lats[rmlat] = np.NaN
             lons[rmlat] = np.NaN
             amf[rmlat]  = np.NaN
-        
+
         # remove solarzenithangle over 60 degrees
         if szamax is not None:
             rmsza       = sza > szamax
@@ -272,7 +272,7 @@ def read_omhcho(path, szamax=60, screen=[-5e15, 1e17], maxlat=None, verbose=Fals
             lats[rmsza] = np.NaN
             lons[rmsza] = np.NaN
             amf[rmsza]  = np.NaN
-        
+
         # remove VCs outside screen range
         if screen is not None:
             # ignore warnings from comparing NaNs to Values
@@ -285,7 +285,7 @@ def read_omhcho(path, szamax=60, screen=[-5e15, 1e17], maxlat=None, verbose=Fals
             lats[rmscr] = np.NaN
             lons[rmscr] = np.NaN
             amf[rmscr]  = np.NaN
-    
+
     #return everything in a structure
     return {'HCHO':hcho,'lats':lats,'lons':lons,'AMF':amf,'AMFG':amfg,
             'omega':w,'apriori':apri,'plevels':plevs, 'cloudfrac':clouds,
@@ -314,7 +314,7 @@ def read_omhcho_8days(day=datetime(2005,1,1)):
     '''
     data8=read_omhcho_day(day)
     for date in [ day + timedelta(days=d) for d in range(1,8) ]:
-        data=read_omhcho_day(date) 
+        data=read_omhcho_day(date)
         for key in data.keys():
             axis= [0,1][key in ['omega','apriori','plevels']]
             data8[key] = np.concatenate( (data8[key], data[key]), axis=axis)
@@ -324,7 +324,7 @@ def read_omhchorp(date, oneday=False, latres=0.25, lonres=0.3125, keylist=None, 
     '''
     Function to read a reprocessed omi file, by default reads an 8-day average (8p)
     Inputs:
-        date = datetime(y,m,d) of file 
+        date = datetime(y,m,d) of file
         oneday = False : read a single day average rather than 8 day average
         latres=0.25
         lonres=0.3125
@@ -333,7 +333,7 @@ def read_omhchorp(date, oneday=False, latres=0.25, lonres=0.3125, keylist=None, 
     Output:
         Structure containing omhchorp dataset
     '''
-    
+
     if keylist is None:
         keylist=_omhchorp_keylist
     retstruct=dict.fromkeys(keylist)
@@ -341,7 +341,7 @@ def read_omhchorp(date, oneday=False, latres=0.25, lonres=0.3125, keylist=None, 
         fpath=determine_filepath(date,oneday=oneday,latres=latres,lonres=lonres,reprocessed=True)
     else:
         fpath=filename
-    
+
     with h5py.File(fpath,'r') as in_f:
         #print('reading from file '+fpath)
         for key in keylist:
@@ -375,7 +375,7 @@ def filter_high_latitudes(array, latres=0.25, lonres=0.3125, highest_lat=60.0):
 
 def read_AMF_pp(date=datetime(2005,1,1),troprun=True):
     '''
-    Read AMF created by randal martin code for this day 
+    Read AMF created by randal martin code for this day
     along with pixel index for adding data to the good pixel list
     '''
     import os.path
@@ -410,10 +410,10 @@ def read_GC_output(date=datetime(2005,1,1), Isop=False,
     keys=[tropchem_HCHO_keys, UCX_HCHO_keys][UCX]
     if Isop:
         keys=[tropchem_Isop_keys, UCX_Isop_keys][UCX]
-    
+
     data={}
     if UCX:
         data=get_UCX_data(date,keys=keys,surface=surface)
     else:
-        data=get_tropchem_data(date,keys=keys,monthavg=monthavg,surface=surface
+        data=get_tropchem_data(date,keys=keys,monthavg=monthavg,surface=surface)
     return data

plotting.py
@@ -81,35 +81,35 @@ def createmap(data,lats,lons, vmin=None, vmax=None, latlon=True,
     lllat=region[0]; urlat=region[2]; lllon=region[1]; urlon=region[3]
     m=Basemap(llcrnrlat=lllat, urcrnrlat=urlat, llcrnrlon=lllon, urcrnrlon=urlon,
               resolution='i', projection='merc')
-    
+
     # Set vmin and vmax if necessary
     if vmin is None:
         vmin=1.05*np.nanmin(data)
     if vmax is None:
         vmax=0.95*np.nanmax(data)
-    
+
     # fix the lats and lons to 2dimensional meshes(if they're not already)
     if len(lats.shape) == 1:
         lonsnew,latsnew=np.meshgrid(lons,lats)
     else:
         latsnew,lonsnew=(lats,lons)
-    
+
     pcmeshargs={'latlon':latlon, 'vmin':vmin, 'vmax':vmax, 'clim':(vmin,vmax)}
-    if not linear: 
+    if not linear:
         pcmeshargs['norm']=LogNorm()
-    
+
     cs=m.pcolormesh(lonsnew, latsnew, data, **pcmeshargs)
     #, latlon=latlon, vmin=vmin, vmax=vmax, norm=norm, clim=(vmin,vmax))
-    
+
     # colour limits for contour mesh
     cs.cmap.set_under('white')
     cs.cmap.set_over('pink')
     cs.set_clim(vmin,vmax)
-    
+
     # draw coastline and equator(no latlon labels)
     m.drawcoastlines()
-    m.drawparallels([0],labels=[0,0,0,0]) 
-    
+    m.drawparallels([0],labels=[0,0,0,0])
+
     # add title and cbar label
     if ptitle is not None:
         plt.title(ptitle)
@@ -118,14 +118,14 @@ def createmap(data,lats,lons, vmin=None, vmax=None, latlon=True,
         cb=m.colorbar(cs,"bottom",size="5%", pad="2%")
         if clabel is not None:
             cb.set_label(clabel)
-    
+
     # if a plot name is given, save and close figure
     if pname is not None:
         plt.savefig(pname)
         print("Saved "+pname)
         plt.close()
-        return 
-    
+        return
+
     # if no colorbar is wanted then don't return one (can be set externally)
     return m, cs, cb
 
@@ -207,23 +207,26 @@ def plot_corellation(X,Y, lims=[1e12,2e17], logscale=True, legend=True,
 
 def plot_time_series(datetimes,values,ylabel=None,xlabel=None, pname=None, legend=False, title=None, dfmt='%Y%m', **pltargs):
     ''' plot values over datetimes '''
-    plt.plot(datetimes, values, **pltargs)
-    
+    dates = mdates.date2num(datetimes)
+    #plt.plot_date(dates, values, **pltargs)
+    plt.plot(dates,values,**pltargs)
+
     #Handle ticks:
+    #plt.gcf().autofmt_xdate()
     plt.xticks(rotation=55)
     myFmt = mdates.DateFormatter(dfmt)
     plt.gca().xaxis.set_major_formatter(myFmt)
-    
+
     if ylabel is not None:
         plt.ylabel(ylabel)
     if xlabel is not None:
         plt.xlabel(xlabel)
     if legend:
-        plt.legend() 
+        plt.legend()
     if title is not None:
         plt.title(title)
     if pname is not None:
         plt.savefig(pname)
         print('%s saved'%pname)
         plt.close()
-    
+
Updated the units for Inversion and Analysis fix

Inversion.py
@@ -85,16 +85,16 @@ def Emissions_1day(day, GC_biog, OMI, region=pp.__AUSREGION__):
     if __VERBOSE__:
         # Check the dims of our stuff
         print()
-        print("Calculating emissions for %s"%dstr) 
+        print("Calculating emissions for %s"%dstr)
         print("GC data %s "%str(GC.hcho.shape)) # [t,lat,lon,lev]
-        print("nanmean:",np.nanmean(GC.hcho),GC.attrs['hcho']['unit']) # should be molecs/cm2
+        print("nanmean:",np.nanmean(GC.hcho),GC.attrs['hcho']['units']) # should be molecs/cm2
         print("OMI data %s"%str(OMI.VCC.shape)) # [t, lat, lon]
         print("nanmean:",np.nanmean(OMI.VCC),'molecs/cm2')# should be molecs/cm2
 
     omilats0, omilons0 = OMI.lats, OMI.lons
     omi_lats, omi_lons= omilats0.copy(), omilons0.copy()
     omi_SA=OMI.surface_areas # in km^2
-    
+
     if __DEBUG__:
         GC_E_isop=GC_biog.hemco.E_isop_bio
         print("GC_E_isop%s [%s] before LT averaging:"%(str(np.shape(GC_E_isop)),GC_biog.hemco.attrs['E_isop_bio']['units']),np.nanmean(GC_E_isop))
@@ -106,7 +106,7 @@ def Emissions_1day(day, GC_biog, OMI, region=pp.__AUSREGION__):
     if __DEBUG__:
         print("GC_E_isop%s after LT averaging:"%str(np.shape(GC_E_isop)),np.nanmean(GC_E_isop))
         print("    non zero only:",np.nanmean(GC_E_isop[GC_E_isop > 0]))
-        
+
         print("GC_E_isop.shape before and after dateindex")
         print(GC_E_isop.shape)
     GC_E_isop=GC_E_isop[util.date_index(day,GC_days)] # only want one day of E_isop_GC
@@ -120,7 +120,7 @@ def Emissions_1day(day, GC_biog, OMI, region=pp.__AUSREGION__):
         print('shape and mean before and after subsetting GC_E_isop:')
         print(np.shape(GC_E_isop), np.nanmean(GC_E_isop))
     GC_E_isop = util.lat_lon_subset(GC.lats,GC.lons,region=region,data=[GC_E_isop])['data'][0]
-    
+
     if __DEBUG__:
         print(np.shape(GC_E_isop), np.nanmean(GC_E_isop))
 
@@ -165,7 +165,7 @@ def Emissions_1day(day, GC_biog, OMI, region=pp.__AUSREGION__):
     slope_after=np.nanmean(GC_slope)
     attrs["GC_slope"]={"units":"s",
         "desc":"\"VC_H=S*E_i+B\" slope (S) between HCHO_GC (molec/cm2) and E_Isop_GC (atom c/cm2/s)"}
-    
+
     with np.errstate(divide='ignore', invalid='ignore'):
         check=100.0*np.abs((slope_after-slope_before)/slope_before)
 
@@ -468,15 +468,15 @@ def store_emissions(day0=datetime(2005,1,1), dayn=None,
     # save each month seperately
     #
     for month in months:
-        
+
         # Grab reprocessed data for the month:
         OMI=omhchorp(day0=month,dayn=util.last_day(month), ignorePP=ignorePP)
         print('mean HCHO column [molec/cm2] from omhchorp',np.nanmean(OMI.VCC))
-        
+
         # Read GC month:
         GC=GC_class.GC_biogenic(month)
         print('mean surface hcho [ppb] from GC_biogenic run:',np.nanmean(GC.sat_out.hcho[:,:,:,0]))
-        
+
         # save the month of emissions
         store_emissions_month(month=month, GC=GC, OMI=OMI,
                               region=region, ignorePP=ignorePP)
@@ -490,7 +490,7 @@ def smearing(month, plot=False,region=pp.__AUSREGION__,thresh=0.0):
         S = d column_HCHO / d E_isop
         For now uses tavg instead of overpass times
     '''
-    if __VERBOSE__: 
+    if __VERBOSE__:
         print('calculating smearing over ',region,' in month ',month)
 
     full=GC_class.GC_tavg(month, run='tropchem')

classes/GC_class.py
@@ -144,12 +144,18 @@ class GC_base:
 
                 attrs[key]['units']='molec/m3'
 
+            nkey=key
             if key in _GC_names_to_nice.keys():
-                setattr(self, _GC_names_to_nice[key], arr)
-                self.attrs[_GC_names_to_nice[key]] = attrs[key]
-            else:
-                setattr(self, key, arr)
-                self.attrs[key]=attrs[key]
+                nkey=_GC_names_to_nice[key]
+            setattr(self, nkey, arr)
+            # just use 'units' not 'unit'
+            if 'unit' in attrs[key]:
+                attrs[key]['units'] = attrs[key]['unit'] # save unit to units
+                attrs[key].pop('unit',None) # delete 'unit' attribute
+
+            self.attrs[nkey] = attrs[key]
+
+
 
             if __VERBOSE__:
                 print("GC_tavg reading %s %s"%(key,data[key].shape))
@@ -162,10 +168,11 @@ class GC_base:
         # molec/cm2 = ppbv * 1e-9 * molec_A / cm3 * H(cm)
         if hasattr(self,'hcho'):
             n_dims=len(np.shape(self.hcho))
-            print("CHECK:hcho dims = %s, hcho=%.2e"%(str(np.shape(self.hcho)),np.mean(self.hcho)))
+            print("CHECK:hcho %s, mean = %.2e %s"%(str(np.shape(self.hcho)),np.mean(self.hcho),self.attrs['hcho']['units']))
             self.O_hcho = np.sum(self.ppbv_to_molec_cm2(keys=['hcho',])['hcho'],axis=n_dims-1)
-            print("CHECK:O_hcho dims = %s, O_hcho=%.2e"%(str(self.O_hcho.shape),np.mean(self.O_hcho)))
             self.attrs['O_hcho']={'units':'molec/cm2','desc':'Total column HCHO'}
+            print("CHECK:O_hcho %s, mean=%.2e %s"%(str(self.O_hcho.shape),np.mean(self.O_hcho),self.attrs['O_hcho']['units']))
+
 
         # Convert from numpy.datetime64 to datetime
         if not hasattr(self,'time'):
@@ -540,6 +547,11 @@ class Hemco_diag(GC_base):
         gram_per_molec = util.__grams_per_mole__['carbon'] / N_Avogadro
         self.kgC_per_m2_to_atomC_per_cm2 = 1e3 * 1e-4 / gram_per_molec
 
+        # make E_isop_bio into atom C/cm2/s
+        assert self.attrs['E_isop_bio']['units']=='kgC/m2/s', 'E_isop_bio units are %s'%self.attrs['E_isop_bio']['units']
+        self.E_isop_bio = self.E_isop_bio*self.kgC_per_m2_to_atomC_per_cm2
+        self.attrs['E_isop_bio']['units']='atomC/cm2/s'
+
         if __VERBOSE__:
             for k in data:
                 print('  %10s %10s %s'%(k, data[k].shape, attrs[k]))
@@ -592,7 +604,8 @@ class Hemco_diag(GC_base):
 
         self.E_isop_bio_LT=out
         self.attrs['E_isop_bio_LT']={'desc':'map for each day of global data at specific hour local time',
-                                     'hour':hour}
+                                     'hour':hour,
+                                     'units':self.attrs['E_isop_bio']['units']}
         return days, np.squeeze(out)
 
     def plot_daily_emissions_cycle(self,lat=-31,lon=150,pname=None):
@@ -679,7 +692,10 @@ class GC_biogenic:
         sat_out=self.sat_out
 
         # grab satellite overpass E_isop and trop column hcho
-        days,isop = megan.daily_LT_averaged(hour=overpass_hour) # lat/lon kgC/cm2/s [days,lat,lon]
+        days,isop = megan.daily_LT_averaged(hour=overpass_hour) # lat/lon kgC/cm2/s [days,lat,lon]?
+        # isop should be atom C/cm2/s
+        assert megan.attrs['E_isop_bio']['units'] == 'atomC/cm2/s', 'units are bad in E_isop_bio %s'%megan.attrs['E_isop_bio']['units']
+
         hcho = sat_out.get_trop_columns(keys=['hcho'])['hcho'] # ppbv -> molec/cm2
 
         # what lats and lons do we want?
@@ -694,9 +710,12 @@ class GC_biogenic:
         sublats, sublons = lats[lati], lons[loni]
 
         # Convert to atomC/cm2/s
-        isop=isop * self.hemco.kgC_per_m2_to_atomC_per_cm2
+        #isop=isop * self.hemco.kgC_per_m2_to_atomC_per_cm2
+        #assert self.hemco.attrs['E_isop_bio']['units'] == 'kgC/cm2/s', 'units are bad for E_isop_bio (%s)'%self.hemco.attrs['E_isop_bio']
 
-        print("nanmeans in slope function (E_isop and trop_hcho in atomC/cm2[/s]):",np.nanmean(isop),np.nanmean(hcho))
+        print("in slope function: ")
+        print("  nanmean E_isop = %.2e %s"%(np.nanmean(isop),'atom C/cm2/s'))
+        print("  nanmean trop_hcho = %.2e %s"%(np.nanmean(hcho),'molec/cm2'))
 
         # arrays to hold the month's slope, background, and regression coeff
         n_x = len(loni)

localtests.py
@@ -51,101 +51,11 @@ diagfile='Data/GC_Output/geos5_2x25_tropchem/satellite_output/diaginfo.dat'
 Hemco_diag="Data/GC_Output/geos5_2x25_tropchem_biogenic/Hemco_diags/E_isop_biog.200501010100.nc"
 biosat_files="Data/GC_Output/geos5_2x25_tropchem_biogenic/satellite_output/sat_biogenic.%s*.bpch"%yyyymm
 
-#dat,attr=GC_fio.read_bpch(path=biosat_files,keys=GC_fio.__sat_mainkeys__,multi=True)
-#dat['IJ-AVG-$_CH2O'].shape
+###
+# read biogenic output
+GC=GC_class.GC_biogenic(d0)
+MS=GC.model_slope()
+OMI=omhchorp(d0,)
 
-#Inversion.store_emissions_month(d1)
-
-# test GC_tavg plotting
-GC=GC_class.GC_sat(d0,)
-
-#plt.subplot(121)
-#pp.createmap(GC.O_hcho[0],GC.lats,GC.lons,aus=True,GC_shift=True,
-#             title='O_hcho',clabel=GC.attrs['O_hcho']['units'])
-#plt.subplot(122)
-#pp.createmap(GC.N_air[0,:,:,0],GC.lats,GC.lons,aus=True,GC_shift=True,
-#             clabel=GC.attrs['N_air']['units'], title='N_air',
-#             linear=True)
-
-# READ OMI
-month=d0
-dayn=util.last_day(month)
-OMI=omhchorp(month,dayn=dayn)
-
-# Check data
-print ('OMI (VCC) molec/cm2',OMI.VCC.shape)
-OMIhcho=OMI.time_averaged(month,dayn,keys=['VCC'])['VCC']# molec/cm2
-print('month average globally:',np.nanmean(OMIhcho))
-
-print("GC (O_hcho)",GC.attrs['O_hcho']['units'], GC.O_hcho.shape)
-GChcho=np.nanmean(GC.O_hcho,axis=0) # time averaged for the month
-print("month average globally:",np.nanmean(GChcho))
-
-plt.figure(figsize=(12,12))
-plt.subplot(221)
-pp.createmap(GChcho,GC.lats,GC.lons,aus=True,GC_shift=True, linear=True,
-             vmin=0,vmax=2e16, cmapname='rainbow',
-             title=r'GC $\Omega_{HCHO}$', clabel=r'molec cm$^{-2}$')
-
-plt.subplot(222)
-pp.createmap(OMIhcho,OMI.lats,OMI.lons,aus=True, linear=True,
-             vmin=0,vmax=2e16, cmapname='rainbow',
-             title=r'OMI $\Omega_{HCHO}$',clabel=r'molec cm$^{-2}$')
-
-# regrid GChcho onto higher resolution
-lats,lons=OMI.lats,OMI.lons
-GChcho=util.regrid(GChcho,GC.lats,GC.lons,lats,lons)
-
-diff=GChcho-OMIhcho
-rdiff=(GChcho-OMIhcho)/OMIhcho
-
-plt.subplot(212)
-pp.createmap(diff,lats,lons, region=[-50,100,-5,170], GC_shift=True, linear=True,
-             vmin=-1.5e16, vmax=1.5e16, cmapname='seismic',
-             title='GC - OMI')
-
-#plt.subplot(224)
-#pp.createmap(rdiff,lats,lons, aus=True, GC_shift=True, linear=True,
-#             vmin=-1.5, vmax=1.5,cmapname='seismic',
-#             title='(GC - OMI)/OMI')
-#plt.suptitle("GEOS-Chem vs OMI total column HCHO")
-plt.tight_layout()
-plt.subplots_adjust(hspace=0.2)
-pname='Figs/GC_vs_OMI_hcho.png'
-plt.savefig(pname)
-print("SAVED ",pname)
-plt.close()
-
-# test stupid LT avg function
-#GC=GC_class.Hemco_diag(d0)
-#days,isop=GC.daily_LT_averaged()
-#
-#isop=isop*GC.kgC_per_m2_to_atomC_per_cm2
-#np.nanmean(isop)
-#m,cs,cb=pp.basicmap(isop,GC.lats,GC.lons,linear=True)
-#cs.set_clim(0,np.nanmax(isop)*0.9)
-#GC=GC_class.GC_sat(d0,run='biogenic')
-#GC=GC_class.GC_biogenic(d0)
-#print(GC)
-
-#
-#dat,att=GC_fio.read_Hemco_diags(d0)
-#e=dat['ISOP_BIOG']
-#lons=dat['lon']
-#a=att['ISOP_BIOG']
-#e.shape # time, lat, lon
-#np.nanmean(e)
-#for k in a:
-#    print(k,':',a[k])
-#for k in dat:
-#    print(k, ':', dat[k].shape)
-#
-#
-#hd=GC_class.Hemco_diag(d0,month=False)
-##hd.plot_daily_emissions_cycle()
-#days,E_megan=hd.daily_LT_averaged()
-#lats=hd.lats
-#lons=hd.lons
-#pp.basicmap(E_megan,lats,lons,aus=True,pname='test.png')
-#print(np.nanmean(E_megan))
-
+enew=Inversion.Emissions_1day(d0,GC,OMI)
+enew.keys()
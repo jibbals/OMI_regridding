8c62ad75c98e30b9ee8876d1f2907e2b317e75cd
Jesse Greenslade <jwg366@uowmail.edu.au>
2017-12-05 21:12:38 +1100

    Added a couple of figures and a script

---
+++ run/inversion_year.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "EG 1: $0 2005"
+    echo "   to save E_new fOR 2005"
+    exit 0
+fi
+
+for i in `seq 1 12`;
+do
+    printf -v mm "%02d" $i
+    yyyymm=$1$mm
+
+    qsub -o logs/inversion_${yyyymm} -v MONTH=${yyyymm} run/inversion_month.sh
+done
+

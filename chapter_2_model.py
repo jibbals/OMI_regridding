0a899dfacfcbb3900bcb019d9230efadad426c7c
Jesse Greenslade <bboyjesse@gmail.com>
2019-05-24 17:45:03 +1000

    updated pyrogenic filter plot and typical aaod plot

--- chapter_2_model.py
+++ chapter_2_model.py
@@ -303,143 +303,6 @@ def plot_VCC_firefilter_vs_days(month=datetime(2005,1,1),region=pp.__AUSREGION__
         also different fire filtering strengths
               |  VCC_pp | fires filtered
         fire0 |
-        fire1 |
-        fire2 |
-        fire4 |
-        fire8 |
-    '''
-    
-    d0 = util.first_day(month)
-    dn = util.last_day(month)
-    # start by reading all the VCC stuff
-    # useful strings
-    ymstr=d0.strftime('%Y%m')
-    pname='Figs/VCC_fires_%s.png'%ymstr
-    #pname2='Figs/VCC_entries_%s.png'%ymdstr
-    vmin,vmax=4e15,9e15 # min,max for colourbar
-    linear=True # linear colour scale?
-    vmin2,vmax2=0,40
-    
-    # read in omhchorp
-    om=omhchorp(d0,dayn=dn, keylist=['VCC_PP','ppentries'])
-    
-    VCC = om.VCC_PP
-    pix = om.ppentries
-    subsets=util.lat_lon_subset(om.lats,om.lons,region=region,data=[VCC,pix], 
-                                has_time_dim=True)
-    VCC,pix = subsets['data'] # subsetted to region
-    lats=subsets['lats']
-    lons=subsets['lons']
-    
-    #print(vars(om).keys()) # [3, 720, 1152] data arrays returned, along with lats/lons etc.
-    oceanmask=util.oceanmask(lats,lons)# lets just look at land squares
-    oceanmask3d = np.repeat(oceanmask[np.newaxis,:,:], np.shape(VCC)[0], axis=0)
-    VCC[oceanmask3d] = np.NaN
-    pix[oceanmask3d] = 0
-    
-    
-    # clear some ram
-    del om
-    
-    # Plot rows,cols,size:
-    f,axes=plt.subplots(5,2,figsize=[12,14])
-    
-    # second plot just for entries
-    # f2, axes2=plt.subplots(5,3,figsize=[18,18])
-
-    # first line is maps of VCC, VC_GC, VCC_PP
-    titles=["VCC_{PP}", "$N_{pixels}$"]
-    
-    # Now loop over the same plots after NaNing our different fire masks
-    for j, N in enumerate([0,1,2,4,8]):
-        
-        # first make the new active fire mask
-        # firemask is 3dimensional: [days,lats,lons]
-        fstart=timeit.default_timer()
-        if j==0:
-            firemask=np.zeros(np.shape(VCC)).astype(np.bool)
-        else:
-            firemask, fdays, flats, flons, fires=fio.make_fire_mask(d0,dN=dn,prior_days_masked=N,region=region)
-        felapsed = timeit.default_timer() - fstart
-        print ("TIMEIT: Took %6.2f seconds to make_fire_mask(%d days)"%(felapsed,N))
-        
-        # appply mask to VCC and pixels
-        VCCj = np.copy(VCC)
-        pixj = np.copy(pix)
-        VCCj[firemask] = np.NaN
-        pixj[firemask] = 0
-        
-        # Then plot VCC and entries maps
-        plt.sca(axes[j,0])
-        
-        # VCC first:
-        m,cs,cb= pp.createmap(np.nanmean(VCCj,axis=0),lats,lons,
-                              region=region, linear=linear,
-                              vmin=vmin,vmax=vmax,
-                              cmapname='rainbow',colorbar=False)
-        if j==0:
-            plt.title(titles[0])
-
-        # add a little thing showing entries and mean and max
-        txt=['N($\mu$)=%d(%.1f)'%(np.nansum(pixj),np.nanmean(pixj)), '$\mu$ = %.2e'%np.nanmean(VCCj), 'max = %.2e'%np.nanmax(VCCj)]
-        for txt, yloc in zip(txt,[0.01,0.07,0.13]):
-            plt.text(0.01, yloc, txt,
-                 verticalalignment='bottom', horizontalalignment='left',
-                 transform=plt.gca().transAxes,
-                 color='k', fontsize=10)
-
-        # also plot entries
-        plt.sca(axes[j,1])
-        m2, cs2, cb2 = pp.createmap(np.nanmean(pixj,axis=0),lats,lons,
-                     region=region, linear=True,
-                     cmapname='jet', colorbar=False,
-                     vmin=vmin2,vmax=vmax2)
- 
-        if j==0:
-            plt.title(titles[1])
-
-    # Add row labels
-    rows = ['%d days'%fdays for fdays in [0,1,2,4,8]]
-    rows[0]='No filter applied'
-    for ax, row in zip(axes[:,0], rows):
-        ax.set_ylabel(row, rotation=0, size='small')
-
-    # Need to add colour bar for left column and right column
-    # left column:
-    ticks=[np.logspace(np.log10(vmin),np.log10(vmax),5),np.linspace(vmin,vmax,5)][linear]
-    pp.add_colourbar(f,cs,ticks=ticks,label='molec/cm$^2$')
-    #pp.add_colourbar(f2,cs2,ticks=np.linspace(vmin2,vmax2,5),label='pixels')
-
-    f.savefig(pname)
-    plt.close(f)
-    print("Saved ",pname)
-    
-    
-
-###########################
-#### MAIN
-###########################
-
-
-if __name__=="__main__":
-    
-    
-    ##############################
-    #### OMI RECALC PLOTS ########
-    
-    # AMF distribution summary 
-    #AMF_distributions()
-    #AMF_distributions(VCCs=True)
-    
-    
-    month=datetime(2005,1,1)
-    region=pp.__AUSREGION__
-    '''
-        Plot columns with different amf bases
-        also different fire filtering strengths
-              |  VCC_pp | fires filtered
-        fire0 |
-        fire1 |
         fire2 |
         fire4 |
         fire8 |
@@ -574,4 +437,91 @@ if __name__=="__main__":
     plt.close(f)
     print("Saved ",pname)
     
+
+def pyrogenic_filter():
+    '''
+        Show pyrogenic filter pixels removed for 2005 and 2012, and time series
+    '''
+    pname = "Figs/Filters/Pyrogenic_filter.png"
+    d0=datetime(2005,1,1)
+    dN=datetime(2005,12,31)
+    
+    # Also want to look at 2012
+    d12 = datetime(2012,1,1)
+    d12N= datetime(2012,12,31)
+    
+    firemask05, dates05, lats, lons  = fio.get_fire_mask(d0,dN,region=pp.__AUSREGION__)
+    firemask12, dates12, lats, lons  = fio.get_fire_mask(d12,d12N,region=pp.__AUSREGION__)
+    smokemask05, _, _, _             = fio.get_smoke_mask(d0,dN,region=pp.__AUSREGION__)
+    smokemask12, _, _, _             = fio.get_smoke_mask(d12,d12N,region=pp.__AUSREGION__)
+    
+    pyro05 = firemask05+smokemask05
+    pyromap05 = np.nansum(pyro05,axis=0).astype(np.float)
+    pyro12 = firemask12+smokemask12
+    pyro12 = pyro12[0:-1] # cut off that leap year day
+    pyromap12 = np.nansum(pyro12,axis=0).astype(np.float)
+    
+    
+    plt.close()
+    #plt.figure(figsize=[16,16])
+    plt.subplot(2,2,1)
+    m,cs,cb    = pp.createmap(pyromap05,lats,lons,title='days filtered 2005',
+                              aus=True,linear=True, set_under='grey',vmin=1, vmax=300,colorbar=False)
+    plt.subplot(2,2,2)
+    m2,cs2,cb2 = pp.createmap(pyromap12,lats,lons,title='days filtered 2012',
+                              aus=True,linear=True, set_under='grey',vmin=1, vmax=300, colorbar=False)
+    
+    plt.subplot(2,1,2)
+    
+    # Take out ocean squares
+    om = util.oceanmask(lats,lons)
+    om = np.repeat(om[np.newaxis,:,:],365,axis=0)
+    squares = np.ones(om.shape).astype(np.float)
+    squares[om] = np.NaN
+    for arr, label, color in zip([pyro05,pyro12],['2005','2012'],['saddlebrown','magenta']):
+        arr = arr.astype(np.float)
+        arr[om]=np.NaN
+        # portion of gridsquares filtered out as a percentage of land squares available
+        ts = 100 * np.nansum(arr,axis=(1,2))/np.nansum(squares,axis=(1,2))
+        plt.plot(np.arange(1,len(ts)+1), ts, linewidth=2, label=label, color=color)
+    plt.title('land area filtered')
+    plt.legend(loc='best')
+    plt.ylabel('%')
+    plt.xlabel('Day of the year')
+    
+    # add axis to middle area just below maps
+    # left, bottom, width, height
+    axes=[0.33,0.54,0.33,0.03]
+    f=plt.gcf()
+    cbar_ax = f.add_axes(axes)
+    cb=f.colorbar(cs,cax=cbar_ax,orientation='horizontal')
+    #cb.set_ticks(ticks)
+    cb.set_label('days masked')
+    
+    plt.savefig(pname)
+    print('Saved ',pname)
+            
+
+###########################
+#### MAIN
+###########################
+
+
+if __name__=="__main__":
+    
+    fullpageFigure()
+    ##############################
+    #### OMI RECALC PLOTS ########
+    
+    # AMF distribution summary 
+    #AMF_distributions()
+    #AMF_distributions(VCCs=True)
+    
+    
+    ##############################
+    ### FIltering stuff 3#########
+    #N_pixels_comparison()
+    #plot_VCC_firefilter_vs_days()
+    #pyrogenic_filter() # 2019/5/24
+     
     
